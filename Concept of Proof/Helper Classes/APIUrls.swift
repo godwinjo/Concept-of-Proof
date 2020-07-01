736ac1fd1d2b4932d2308d7f9bb362672cbac5e1
Godwin <you@example.com>
2020-07-01 11:56:19 +0530

    Added two helper classes for handling and managing API calls

---
+++ Concept of Proof/Helper Classes/APIUrls.swift
@@ -0,0 +1,17 @@
+//
+//  APIUrls.swift
+//  Concept of Proof
+//
+//  Created by Godwin on 30/06/20.
+//  Copyright © 2020 Godwin. All rights reserved.
+//
+
+import Foundation
+
+//MARK:- API list
+enum APIUrl:String {
+    case listApi = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
+
+
+
