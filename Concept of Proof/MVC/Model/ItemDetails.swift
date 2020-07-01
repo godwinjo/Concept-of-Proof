599cd01f760240c55a925cec98b9fc35fdac88f8
Godwin <you@example.com>
2020-07-01 12:05:19 +0530

    Model classes created for managing API response

---
+++ Concept of Proof/MVC/Model/ItemDetails.swift
@@ -0,0 +1,42 @@
+//
+//  ItemDetails.swift
+//  Concept of Proof
+//
+//  Created by Godwin on 30/06/20.
+//  Copyright © 2020 Godwin. All rights reserved.
+//
+
+import UIKit
+
+
+class Item: NSObject {
+    
+    var itemTitle :   String?
+    var itemDetails : [ItemDetails]?
+    
+    init(dict:NSDictionary) {
+        self.itemTitle = dict["title"] as? String
+        if let rows = dict["rows"] as? NSArray {
+            itemDetails = [ItemDetails]()
+            for row in rows {
+                let itemDetail = ItemDetails(dict: row as! NSDictionary)
+                itemDetails?.append(itemDetail)
+            }
+        }
+    }
+
+}
+
+class ItemDetails: NSObject {
+    
+    var itemTitle :        String?
+    var itemDescription : String?
+    var itemImage :       String?
+    
+    init(dict:NSDictionary) {
+        self.itemTitle = dict["title"] as? String
+        self.itemDescription = dict["description"] as? String
+        self.itemImage = dict["imageHref"] as? String
+    }
+
+}
